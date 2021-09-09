dc7c897331f54d032e970ef55b50139c52e32c8e
Abhi <abhidadhaniya23@gmail.com>
2021-09-09 23:59:38 +0530

    update navbar and styles

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,7 +1,10 @@
 // console.log('linked');
 const navBtn = document.getElementById('navBtn');
 navBtn.addEventListener('click', () => {
+    console.log('Clicked');
+
     // console.log('btn clicked');
-    document.querySelector('nav').classList.toggle('navbar-active');
+    document.querySelector('.navDiv').classList.toggle('navbar-active');
+    document.getElementById('navBar').classList.toggle('links-active');
     // document.querySelector('nav').classList.replace('-top-40','top-24');
 })
