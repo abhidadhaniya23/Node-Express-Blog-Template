2f20c4d12c9198022b34a64ac335adf935148d04
Abhi <abhidadhaniya23@gmail.com>
2021-09-10 16:38:23 +0530

    fixed mobile screen navbar animations and all

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,10 +1,8 @@
 // console.log('linked');
 const navBtn = document.getElementById('navBtn');
-navBtn.addEventListener('click', () => {
-    console.log('Clicked');
 
-    // console.log('btn clicked');
+navBtn.addEventListener('click', () => {
     document.querySelector('.navDiv').classList.toggle('navbar-active');
     document.getElementById('navBar').classList.toggle('links-active');
-    // document.querySelector('nav').classList.replace('-top-40','top-24');
+    document.querySelectorAll('.nav-link').classList.toggle('show-links');
 })
